68728bf2002e1dd7ae211448b5fada882103843e
Anthony Haslett <Anthony-Haslett@outlook.com>
2019-06-09 15:30:48 +0100

    refactor(MVC): adding in barebones MVC project files

---
+++ ClubOneMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClubOneMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
